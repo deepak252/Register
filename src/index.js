d82349e667b514ed94432fdd869e0f4cc9d7ff5a
Deepak Chaurasiya <72331440+deepak252@users.noreply.github.com>
2021-03-06 20:51:33 +0530

    Starting Files

--- src/index.js
+++ src/index.js
@@ -9,3 +9,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 //Show Register as the button text if userIsRegistered is false.
 //2. Only show the Confirm Password input if userIsRegistered is false.
 //Don't show it if userIsRegistered is true.
+//
